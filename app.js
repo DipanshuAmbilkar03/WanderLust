d2e0078880628ba74fcb8c6a1bde8a97d9ce6147
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2024-10-18 23:32:16 +0530

    logging

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// REQUIREMENTS
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
@@ -6,9 +7,10 @@ const port = 8080;
 const methodOverride = require('method-override');
 const ejsMate = require("ejs-mate");
 
-const wrapAsync = require("./utils/wrapAsync.js");
 const Listing = require("./models/listing.js"); 
 
+const wrapAsync = require("./utils/wrapAsync.js");
+const expressError = require("./utils/expressError.js");
 
 let MONGO_URL = 'mongodb://127.0.0.1:27017/WanderLust';
 
@@ -31,62 +33,77 @@ async function main() {
     await mongoose.connect(MONGO_URL);
 }
 
-app.get("/listings" , async (req,res) => {
+app.get("/listings" , wrapAsync(async (req,res) => {
     const allListing  = await Listing.find({});
     res.render('./listings/index.ejs', { allListing });
 
     // Listing.find({}).then(res => {
     //     console.log(res);
     // })
-})
+}))
 
 app.get("/listings/new", (req,res) => {
     res.render("./listings/new.ejs")
 })
 
 
-app.get("/listings/:id",async (req,res) => {
+app.get("/listings/:id",wrapAsync(async (req,res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("./listings/show.ejs" , {listing});
-})
+}))
 
 // create post request
-app.post("/listings", async(req,res,next) => {
-    try {
-        const newListing = new Listing(req.body.listing);
-        await newListing.save();
-        res.redirect("/listings");
-    } catch (err) {
-        next(err);
-    }
-})
 
-app.get("/listings/:id/edit" , async (req,res) => {
+// error handling
+// app.post("/listings", async(req,res,next) => {
+//     try {
+//         const newListing = new Listing(req.body.listing);
+//         await newListing.save();
+//         res.redirect("/listings");
+//     } catch (err) {
+//         next(err);
+//     }
+// })
+
+// server side error handling 
+app.post("/listings", wrapAsync(async(req,res,next) => {
+    if(!req.body.listing) {
+        throw new expressError(400 , "Please provide a listing.");
+    }
+    
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+}))
+
+app.get("/listings/:id/edit" , wrapAsync(async (req,res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
     res.render("./listings/edit.ejs" , { listing })
-})
+}))
 
-app.get("/listings/:id/delete" , async (req,res) => {
+app.get("/listings/:id/delete" , wrapAsync(async (req,res) => {
     let { id } = req.params;
     let deletedListing = await Listing.findByIdAndDelete(id);
     console.log(deletedListing);
     res.redirect(`/listings`);
-})
+}))
 
-app.put("/listings/:id" , async (req,res) => {
+app.put("/listings/:id" , wrapAsync(async (req,res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id , {...req.body.listing});
     res.redirect(`/listings/${id}`);
-})
+}))
+
 //  <------------------------------------------------------------------>
 // test delete this later
-app.post("/listings/:id/test" , async (req , res) => {
+app.post("/listings/:id/test" , wrapAsync(async (req , res) => {
     let { id } = req.params;
     let listing = await Listing.findById(id);
     res.render("./listings/test.ejs" , {listing});
-})
+}))
+
 //  <------------------------------------------------------------------>
 
 app.get("/", (req,res) => {
@@ -111,8 +128,29 @@ app.get("/", (req,res) => {
 //     res.send("successfully tested")
 // });
 
+// error for an entirly different api route
+app.all("*" , (req, res, next) => {
+    next(new expressError("404" , "Page Not Found!"))
+})
+
+// error middleware
+// app.use((err, req, res, next) => {
+//     res.send("something failed")
+// })
+
+// error middleware using expressclass
 app.use((err, req, res, next) => {
-    res.send("something failed")
+    let {statusCode=500, message="something went wrong"} = err;
+    // res.status(statusCode).send(message);
+
+    // for message handling
+    res.render("error.ejs",{err});
+
+    // for error handling
+    res.render("error.ejs", {err});
+
+    // for random error handling
+    // res.send("something failed");
 })
 
 
