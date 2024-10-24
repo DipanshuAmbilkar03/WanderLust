38675d284eb655756769b58982c40fc19581a2aa
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2024-10-24 18:56:48 +0530

    joi package

---
+++ schema.js
@@ -0,0 +1,12 @@
+const Joi = require('joi');
+
+module.exports.listingSchema = Joi.object({
+    listing : Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string(),
+        image: Joi.string().allow("",null),
+        price: Joi.number().min(0).required(),
+        location: Joi.string().required(),
+        country: Joi.string().required()
+    }).required()
+});
