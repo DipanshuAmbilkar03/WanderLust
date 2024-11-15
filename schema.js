0b3f0b8f9b713360f4f03b05181e4b74e73523b4
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2024-11-15 19:33:21 +0530

    joi sucks

--- schema.js
+++ schema.js
@@ -1,17 +1,15 @@
-// const Joi = require('joi');
+const Joi = require('joi');
 
-// module.exports.listingSchema = Joi.object({
-//     listing: Joi.object({
-//         title: Joi.string().required(),
-//         description: Joi.string().required(),
-//         image: Joi.object({
-//             URL: Joi.string().uri().allow("", null)
-//         }).optional(),
-//         price: Joi.number().min(0).required(),
-//         location: Joi.string().required(),
-//         country: Joi.string().required(),
-//     }).required()  
-// });
+module.exports.listingSchema = Joi.object({
+    listing: Joi.object({
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        image: Joi.string().uri().allow("",null).optional(),        
+        price: Joi.number().min(0).required(),
+        location: Joi.string().required(),
+        country: Joi.string().required(),
+    }).required()  
+});
 
 
 // const Joi = require('joi');
@@ -27,18 +25,18 @@
 //     }).required()  
 // });
 
-const Joi = require('joi');
+// const Joi = require('joi');
 
-const listingSchema = Joi.object({
-    listing: Joi.object({
-        title: Joi.string().required(),
-        description: Joi.string().required(),
-        location: Joi.string().required(),
-        price: Joi.number().required(),
-        image: Joi.object({
-            URL: Joi.string().uri().optional()
-        }).optional()
-    }).required()
-});
+// const listingSchema = Joi.object({
+//     listing: Joi.object({
+//         title: Joi.string().required(),
+//         description: Joi.string().required(),
+//         location: Joi.string().required(),
+//         price: Joi.number().required(),
+//         image: Joi.object({
+//             URL: Joi.string().uri().optional()
+//         }).optional()
+//     }).required()
+// });
 
-module.exports = { listingSchema };
+// module.exports = { listingSchema };
