018e6140c3ab6498ae7b5973aeb77dc456eae9b1
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2024-11-17 18:51:16 +0530

    Review schema for server-side (SS) validation

--- schema.js
+++ schema.js
@@ -11,6 +11,12 @@ module.exports.listingSchema = Joi.object({
     }).required()  
 });
 
+module.exports.reviewSchema = Joi.object({
+    review : Joi.object({
+        rating: Joi.number().required(),
+        comment: Joi.string().required(),
+    }).required(),
+})
 
 // const Joi = require('joi');
 
