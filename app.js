f69c55aa4f7f421d52f68713e413c67336b59c7d
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2024-11-18 18:23:20 +0530

    new reviews added

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.get("/listings/new", (req,res) => {
 
 app.get("/listings/:id",wrapAsync(async (req,res) => {
     let { id } = req.params;
-    const listing = await Listing.findById(id);
+    const listing = await Listing.findById(id).populate("reviews");
     res.render("./listings/show.ejs" , {listing});
 }))
 
