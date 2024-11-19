cc9f4d556a4f7328314e3b7b0ab1d3ce800891cc
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2024-11-19 20:00:10 +0530

    adding cookies

--- CR/server.js
+++ CR/server.js
@@ -5,10 +5,19 @@ const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
 
+// cookies 
+app.get("/getcookies" , (req, res) => {
+    res.cookie("Greet" , "hello");
+    res.cookie("Warning" , "You are being tracked");
+    res.send("cookies send!");
+})
+
 // root directory
 app.get("/" , (req,res) => {
     res.send("Root directory");
 });
+
+
 
 app.use("/user" ,users);
 app.use("/post" ,posts);
