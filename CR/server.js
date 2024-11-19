c0a180e23ec165ec7922430bf1a13697f7732e2c
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2024-11-19 17:52:47 +0530

    express post routes

--- CR/server.js
+++ CR/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+// user
 app.get("/user" , (req,res) => {
     res.send("Root directory");
 });
@@ -16,6 +17,25 @@ app.post("/user" , (req,res) => {
 
 
 app.delete("/user" , (req,res) => {
+    res.send("Root directory");
+});
+
+// POST
+app.get("/post" , (req,res) => {
+    res.send("Root directory");
+});
+
+app.get("/post/:id" , (req,res) => {
+    res.send("Root directory");
+});
+
+
+app.post("/post" , (req,res) => {
+    res.send("Root directory");
+});
+
+
+app.delete("/post" , (req,res) => {
     res.send("Root directory");
 });
 
