f08488fb21ff1a6e9da9938e505d9e63b1f0555c
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2024-11-19 19:22:07 +0530

    setting mergeParams to [ true ] for using the _:id  from main app.js to the review.js

--- routes/review.js
+++ routes/review.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const router = express.Router();
+const router = express.Router({mergeParams:true});
 const wrapAsync = require("../utils/wrapAsync.js");
 const { reviewSchema } = require("../schema.js");
 const expressError = require("../utils/expressError.js");
