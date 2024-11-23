ff89231327ab7da5e2c40afdd680d02c1b4d743e
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2024-11-23 19:08:10 +0530

    using session for multiple api router

--- CR/server.js
+++ CR/server.js
@@ -6,19 +6,38 @@ const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
 
 const session = require("express-session");
+const sessionOpt = {
+                    secret : "dontlookumfinmybusiness", 
+                    resave : false,
+                    saveUninitialized : true
+                    };
 
+app.use(session(sessionOpt));
 
-app.use(session({secret : "dontlookumfinmybusiness", resave : false,saveUninitialized : true}));
-
-app.get("/getcount" , (req,res) => {
-    if(req.session.count) {
-        req.session.count++;
-    }else{
-        req.session.count = 1;
-    }
-    
-    res.send(`total count is ${req.session.count} times`);
+// register 
+app.get("/register" , (req,res) => {
+    let { name = "anonymous"} = req.query;
+    req.session.name = name;
+    console.log(req.session.name);
+    res.send(name);
 })
+
+// to say hello to the given query name
+app.get("/hello",(req,res) => {
+    
+    res.send(`hello ${req.session.name}`)
+})
+
+// count number of sessions 
+// app.get("/getcount" , (req,res) => {
+//     if(req.session.count) {
+//         req.session.count++;
+//     }else{
+//         req.session.count = 1;
+//     }
+
+//     res.send(`total count is ${req.session.count} times`);
+// })
 
 app.get("/test" , (req,res) => {
     res.send(`this ia a test    `);  
