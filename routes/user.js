8623e4b37f021824688f8b5d77e6d907ea0c5b43
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2024-12-17 20:08:43 +0530

    signUp feature

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router({mergeParams:true});
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync");
 
 router.get("/signup" , (req,res) => {
     res.render("./user/signup.ejs");
 })
+
+router.post("/signup" , wrapAsync(async(req,res) => {
+    try {
+        let { username, email ,password} = req.body;
+        const newUser = new User({username , email});
+        const registeredUser = await User.register(newUser , password);
+        console.log(registeredUser);
+        req.flash("success" , "Welcome to WanderLust");
+        res.redirect("/listings");
+    }catch(e) {
+        req.flash("error" , e.message);
+        res.redirect("/signup");
+    }
+}))
 
 module.exports = router;
