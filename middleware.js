50923484abe900820d6a53a3be5c2c14d005dc6a
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2024-12-19 20:19:58 +0530

    save the original Url which has to be accessed after login

--- middleware.js
+++ middleware.js
@@ -1,6 +1,10 @@
 module.exports.isLoggedIn = (req,res,next) => {
-    console.log(req.user);
+    // console.log(req);
     if(!req.isAuthenticated()) {
+        // when user is not logged in 
+
+        // redirectUrl save
+        req.session.redirectUrl = req.originalUrl;
         req.flash("error" , "you are not logged in");
         return res.redirect("/login");
     }
