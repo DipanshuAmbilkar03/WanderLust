5242cfc38b7c68947080891fdd1ee7f18dc7899a
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2024-12-19 11:32:39 +0530

    logout option

--- routes/user.js
+++ routes/user.js
@@ -36,4 +36,15 @@ router.post("/login",passport.authenticate("local",
         res.redirect("listings");
 }))
 
+router.get("/logout" , (req,res,next) => {
+    req.logout((err) => {
+        if(err) {
+            return next(err);  
+        }
+        req.flash("success" , "You are Logged out");
+        res.redirect("/listings")
+    });
+})
+
+
 module.exports = router;
