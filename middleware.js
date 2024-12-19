4df870657f9930a92983bbd8d985e44f0c9e42f7
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2024-12-19 19:38:50 +0530

    req.user for current status of user

--- middleware.js
+++ middleware.js
@@ -1,4 +1,5 @@
 module.exports.isLoggedIn = (req,res,next) => {
+    console.log(req.user);
     if(!req.isAuthenticated()) {
         req.flash("error" , "you are not logged in");
         return res.redirect("/login");
