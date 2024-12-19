aa75c113650dde99a11242e667a9c89245ea3438
DipanshuAmbilkar03 <dipanshuambilker@gmail.com>
2024-12-19 20:48:20 +0530

    originalUrl saved in locals via saveRedirectUrl middleware ***(res.locals != req.locals)

--- middleware.js
+++ middleware.js
@@ -1,14 +1,18 @@
 module.exports.isLoggedIn = (req,res,next) => {
-    // console.log(req);
+// when user is not logged in 
     if(!req.isAuthenticated()) {
-        // when user is not logged in 
-
         // redirectUrl save
         req.session.redirectUrl = req.originalUrl;
         req.flash("error" , "you are not logged in");
         return res.redirect("/login");
     }
-
     next();
 
 };
+
+module.exports.saveRedirectUrl = (req,res,next) => {
+    if(req.session.redirectUrl) {
+        res.locals.redirectUrl = req.session.redirectUrl;
+    }
+    next();
+}
